9e90d16b6baa58cb01ec6dfe2fc517c455a4238d
vincepay <varghapayandeh@gmail.com>
2018-11-28 00:24:16 -0700

    future work for implementation of variations of quicksort

--- quicksort.py
+++ quicksort.py
@@ -3,6 +3,22 @@ FROM: https://www.quora.com/What-is-an-intuitive-explanation-of-QuickSort
 I liked th explanation and the visuals above. 
 For more thorough understanding of all variations of pivor sort, consider all variations as explained in:
 FROM: https://www.quora.com/How-do-I-analyse-a-Quicksort-algorithm-in-depth
+Record, analyze and graph performance on datasets of varying size created using heuristics such as ascending, descending, monotonic, QuickSort Adversary, Median of 3 Adversary, few unique and any other arrangements imaginable (there are many). To be truly in-depth, examine and test many different algorithms of the Quick family of sorts, including deterministic pivot, randomized pivot, Yaroslavsky dual-pivot, triple pivot, median of 3, median of 9, iterative, recursive and even hybrids like IntroSort, among many other variants you will never find in textbooks. This will then be among what I'd consider pretty well in-depth. Done correctly, this should keep your computer and you busy for some time to come.
+
+Write implementation for :
+  choice of pivot: 
+    mdeterministic pivot
+    randomized pivot
+    Yaroslavsky dual-pivot
+    triple pivot
+    median of 3
+    media of 9
+    
+  iterative
+  recursive
+  hybrids:
+    IntroSort
+
 '''
 import numpy.random as random
 
